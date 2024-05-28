a8b71b873821fbe2703fd9cc62233d46adf26ce5
Jessé Rodrigues da Silva <70242043+jesserodri@users.noreply.github.com>
2024-05-27 22:54:14 -0300

    Update nos comentarios do script-iac2.sh

--- script-iac2.sh
+++ script-iac2.sh
@@ -5,13 +5,17 @@ apt-get upgrade
 apt install apache2 -y
 apt install unzip -y
 
-echo "baixando e configurando..."
+echo "baixando site exemplo e configurando..."
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 cd /var/www/
+
+echo "fazendo backup da pasta HTML"
 mv html html2
+echo "fazendo uma nova pasta"
 mkdir html
 cd /tmp
+echo "descompactando e movendo arquivos"
 unzip main.zip
 mv linux-site-dio-main html
 mv html /var/www/
